d590fa95b748801a672f7b2062f7039c66934ca5
panna001 <sonar.pocket.0926@gmail.com>
2021-04-25 10:52:14 +0000

    [Add] user book コントローラモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
   devise_for :users
+  resources :users, only:[:show, :index, :edit, :update]
+  resources :books, only:[:show, :index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
