b2278cc95fe5dd2d00247b3c9e7c88f0091d9fa9
panna001 <sonar.pocket.0926@gmail.com>
2021-04-26 02:49:21 +0000

    [Add]book一覧

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
+  before_action :set_new_book
+  before_action :set_user
+
   def show
   end
 
   def index
+    @books = Book.all
+
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_new_book
+    @book = Book.new
+  end
+
+  def set_user
+    @user = current_user
   end
 end
