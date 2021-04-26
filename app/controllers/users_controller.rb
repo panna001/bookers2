165d07805f52a52ab057f19082c212c5156c8981
panna001 <sonar.pocket.0926@gmail.com>
2021-04-26 03:21:11 +0000

    [Add]user更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
 
   private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 
   def set_new_book
     @book = Book.new
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '59c50e5547ea0d38624ad2ba6ff0bae78b7ce91f48974ed20d9ab79cab31bb43b3417649a54366a6872de4cd4d348122563e13437fdda8dff8f25de446be4e8c'
