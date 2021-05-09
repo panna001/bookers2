e963c0319830eae3124647affdd4fd5de479a40d
panna001 <sonar.pocket.0926@gmail.com>
2021-05-09 06:06:55 +0000

    [Add]favoriteコントローラ、モデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about'=> "homes#about", as: "homes_about"
+  resources :favorites, only:[:create, :destroy]
   devise_for :users
   resources :users, only:[:show, :index, :edit, :update]
   resources :books, only:[:show, :index, :create, :edit, :update, :destroy]
