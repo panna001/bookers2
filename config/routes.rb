7162a9f6aaf2cc445a4b84f6741758217ddb681b
panna001 <sonar.pocket.0926@gmail.com>
2021-05-09 06:11:15 +0000

    [Fixed]favorite ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about'=> "homes#about", as: "homes_about"
-  resources :favorites, only:[:create, :destroy]
   devise_for :users
   resources :users, only:[:show, :index, :edit, :update]
-  resources :books, only:[:show, :index, :create, :edit, :update, :destroy]
+  resources :books, only:[:show, :index, :create, :edit, :update, :destroy] do
+    resource :favorites, only:[:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
