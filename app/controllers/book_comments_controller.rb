d6e39624a0f33bda2c3960a559719e657b27b2dd
panna001 <sonar.pocket.0926@gmail.com>
2021-05-20 07:27:36 +0000

    [Add]コメント作成非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,15 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(comment_params)
-    @comment.book_id = @book.id
-    @comment.save
-    redirect_back(fallback_location: root_path)
+    @book_detail = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(comment_params)
+    @book_comment.book_id = @book_detail.id
+    @book_comment.save
+    @comments = @book_detail.book_comments
   end
 
   def destroy
     @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
     @comment.destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
